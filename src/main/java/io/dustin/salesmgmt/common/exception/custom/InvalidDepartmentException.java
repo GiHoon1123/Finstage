6bad03625bae5832479decb6a4d7f42bb538ffa9
GiWoonHwang <85157729+GiWoonHwang@users.noreply.github.com>
2025-04-17 09:50:28 +0900

    feat: 커스텀 예외 클래스 구현 (존재하지 않은 부서)

---
+++ src/main/java/io/dustin/salesmgmt/common/exception/custom/InvalidDepartmentException.java
@@ -0,0 +1,8 @@
+package io.dustin.salesmgmt.common.exception.custom;
+
+public class InvalidDepartmentException extends RuntimeException {
+    public  InvalidDepartmentException(String message) {
+        super(message);
+    }
+
+}
