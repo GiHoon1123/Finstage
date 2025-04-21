736985b6d49febe8fa1ea96c07b5e500ad9eb00a
GiWoonHwang <85157729+GiWoonHwang@users.noreply.github.com>
2025-04-21 11:14:08 +0900

    feat: 환경변수 주입을 위한 설정 클래스 구현

---
+++ src/main/java/io/dustin/salesmgmt/common/config/PropertyConfig.java
@@ -0,0 +1,11 @@
+package io.dustin.salesmgmt.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySource("classpath:properties/env-${spring.profiles.active}.properties")
+public class PropertyConfig {
+}
+
