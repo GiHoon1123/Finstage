87a0f3c695168d27159ebb4e8ca0801a745b4a7f
GiWoonHwang <85157729+GiWoonHwang@users.noreply.github.com>
2025-04-26 23:56:41 +0900

    feat: 구체적인 유효성 검사 실패 예외를 위한 공통 응답 객체 수정

--- src/main/java/io/dustin/salesmgmt/common/response/CommonResponse.java
+++ src/main/java/io/dustin/salesmgmt/common/response/CommonResponse.java
@@ -1,7 +1,10 @@
 package io.dustin.salesmgmt.common.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 @Getter
 @RequiredArgsConstructor
@@ -11,11 +14,26 @@ public class CommonResponse<T> {
     private final String message;
     private final T data;
 
+    @JsonInclude(JsonInclude.Include.NON_NULL) // null이면 JSON 응답에서 아예 제외
+    private final List<ValidationError> errors; // 에러 리스트 추가
+
+
     public static <T> CommonResponse<T> success(T data) {
-        return new CommonResponse<>(200, "Success", data);
+        return new CommonResponse<>(200, "Success", data, null);
     }
 
     public static <T> CommonResponse<T> of(int status, String message, T data) {
-        return new CommonResponse<>(status, message, data);
+        return new CommonResponse<>(status, message, data, null);
+    }
+
+    public static <T> CommonResponse<T> of(int status, String message, T data, List<ValidationError> errors) {
+        return new CommonResponse<>(status, message, data, errors);
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    public static class ValidationError {
+        private final String field;
+        private final String message;
     }
 }
