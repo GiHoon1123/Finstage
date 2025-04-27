9cab6a5b1d786fec8926382af768548221c3d7fb
GiWoonHwang <85157729+GiWoonHwang@users.noreply.github.com>
2025-04-28 08:40:51 +0900

    feat: Authorization application/service 테이블에 이미 등록된 회사를 필터링 하기 위한 유스케이스 추가

--- src/main/java/io/dustin/salesmgmt/authorization/application/service/RegisterCompanyAccessService.java
+++ src/main/java/io/dustin/salesmgmt/authorization/application/service/RegisterCompanyAccessService.java
@@ -1,11 +1,13 @@
 package io.dustin.salesmgmt.authorization.application.service;
 
+import io.dustin.salesmgmt.authorization.adapter.out.persistence.LoadUserCompanyAccessPort;
 import io.dustin.salesmgmt.authorization.application.port.in.RegisterCompanyAccessUseCase;
 import io.dustin.salesmgmt.authorization.application.port.in.command.RegisterCompanyAccessCommand;
 import io.dustin.salesmgmt.authorization.application.port.out.RegisterUserCompanyAccessPort;
 import io.dustin.salesmgmt.authorization.domain.Company;
 import io.dustin.salesmgmt.authorization.domain.UserCompanyAccess;
 import io.dustin.salesmgmt.common.annotation.UseCase;
+import io.dustin.salesmgmt.common.exception.custom.AlreadyRegisteredCompanyException;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
@@ -15,18 +17,31 @@ import java.util.List;
 public class RegisterCompanyAccessService implements RegisterCompanyAccessUseCase {
 
     private final RegisterUserCompanyAccessPort registerUserCompanyAccessPort;
+    private final LoadUserCompanyAccessPort loadUserCompanyAccessPort;
 
     @Override
     public boolean register(RegisterCompanyAccessCommand command) {
 
         List<Company> companies = command.getCompanies().stream()
                 .map(c -> Company.of(c.getCompanyId(), c.getCompanyName()))
-                .toList();;
+                .toList();
+
+        UserCompanyAccess existingAccess = loadUserCompanyAccessPort.findExistingCompanyAccesses(
+                command.getUserName(),
+                command.getUserEmail(),
+                companies.stream().map(Company::getCompanyId).toList()
+        );
+
+        if (!existingAccess.getCompanyAccess().isEmpty()) {
+            List<String> duplicatedCompanies = existingAccess.getCompanyAccess().stream()
+                    .map(company -> company.getCompanyId() + ":" + company.getCompanyName()) // ✅ id와 name 같이 보여주기
+                    .toList();
+
+            throw new AlreadyRegisteredCompanyException("이미 등록된 회사가 존재합니다: " + String.join(", ", duplicatedCompanies));
+        }
 
         UserCompanyAccess userCompanyAccess = UserCompanyAccess.of(command.getUserName(), command.getUserEmail(), companies);
-
         registerUserCompanyAccessPort.save(userCompanyAccess);
-
 
         return true;
     }
